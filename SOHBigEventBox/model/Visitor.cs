16dcf5caa3e616b5bdd2d4918472cfb9a335a92b
SimSon <s-schwarzkopf+git@etik.com>
2024-10-28 15:51:19 +0100

    Implement simple not yet multimodal visitor agent

---
+++ SOHBigEventBox/model/Visitor.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Mars.Common.Core.Random;
+using Mars.Interfaces.Environments;
+using SOHModel.Multimodal.Model;
+
+namespace SOHBigEventBox.model;
+
+/// <summary>
+///     This <see cref="Traveler{HumanTravelerLayer}" /> entity uses a variety of modalities to reach its goal.
+/// </summary>
+public class Visitor : Traveler<HumanTravelerLayer>
+{
+    private ISet<ModalChoice> _choices = new HashSet<ModalChoice> { ModalChoice.Walking };
+
+    public override void Init(HumanTravelerLayer layer)
+    {
+        base.Init(layer);
+
+        Gender = (GenderType)RandomHelper.Random.Next(0, 2);
+        OvertakingActivated = false;
+    }
+
+    protected override IEnumerable<ModalChoice> ModalChoices()
+    {
+        return _choices;
+    }
+}
