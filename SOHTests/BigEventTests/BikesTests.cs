e3bead678a5dc3c3d7db0477ed8d4e4f93b5741d
Daniel Glake <daniel.glake@itanker.de>
2024-12-09 13:56:45 +0100

    Update BikeTests to finde the shortest walking route.

--- SOHTests/BigEventTests/BikesTests.cs
+++ SOHTests/BigEventTests/BikesTests.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using Mars.Components.Environments;
 using Mars.Interfaces.Environments;
+using Mars.Interfaces.Model;
+using Mars.Interfaces.Model.Options;
+using SOHModel.Domain.Steering.Acceleration;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,29 +18,33 @@ public class BikesTests
     {
         _output = output;
     }
-
-    public static readonly Position Node1Pos = Position.CreateGeoPosition(9.899291554980337, 53.588794556630731);
-    public static readonly Position Node2Pos = Position.CreateGeoPosition(9.901537343190636, 53.588367979554995);
-
+    
     [Fact]
     public void Test_UnimodalRouteBetweenCoordinates()
     {
         string currentDirectory = Directory.GetCurrentDirectory();
         _output.WriteLine("Current working directory: " + currentDirectory);
-        
-        var environment = new SpatialGraphEnvironment("");
+
+        var environment = new SpatialGraphEnvironment(new Input
+        {
+            File = Path.Combine(
+                "BigEventTests", "resources", "walk_graph_barclays.geojson"),
+            InputConfiguration = new InputConfiguration
+            {
+                GeometryAsNodesEnabled = true,
+                IsBiDirectedImport = true, Modalities = new HashSet<SpatialModalityType>
+                {
+                    SpatialModalityType.Walking
+                }
+            }
+        });
         _output.WriteLine(environment.Nodes.Count.ToString());
-            
-        bool node1Exists = environment.Nodes.Any(node => node.Position.Equals(Node1Pos));
-        Assert.True(node1Exists, "Node1 with the specified position does not exist.");
-        bool node2Exists = environment.Nodes.Any(node => node.Position.Equals(Node2Pos));
-        Assert.True(node2Exists, "Node2 with the specified position does not exist.");
-
-        var startNode = environment.Nodes.First(node => node.Position.Equals(Node1Pos));
-        var goalNode = environment.Nodes.First(node => node.Position.Equals(Node2Pos));
-
-        var route = environment.FindShortestRoute(startNode, goalNode, edge => edge.Modalities.Contains(SpatialModalityType.Walking));
-        Assert.NotNull(route);
-
+        
+        var startNode = environment.NearestNode(new[] { 53.593998, 9.902225 });
+        var goalNode = environment.NearestNode(new[] { 53.5836529, 9.928451 });
+        
+        var selectedRoute = environment.FindShortestRoute(startNode, goalNode,
+            edge => edge.Modalities.Contains(SpatialModalityType.Walking));
+        Assert.NotNull(selectedRoute);
     }
 }
