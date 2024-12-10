7e88c5d0c4d6deb36c870030b8c2157b8fe4828c
René Scheuer <rene.scheuer03@gmail.com>
2024-12-10 08:56:42 +0100

    added an experimental Walking sim for the Christmas Market

--- SOHChristmasMarketBox/Program.cs
+++ SOHChristmasMarketBox/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using Mars.Common.Core.Logging;
+using Mars.Components.Layers;
+using Mars.Components.Starter;
+using Mars.Core.Simulation;
+using Mars.Interfaces;
+using Mars.Interfaces.Model;
+using SOHModel.Bicycle.Model;
+using SOHModel.Bicycle.Parking;
+using SOHModel.Bicycle.Rental;
+using SOHModel.Car.Model;
+using SOHModel.Car.Parking;
+using SOHModel.Car.Rental;
+using SOHModel.Domain.Graph;
+using SOHModel.Multimodal.Model;
 
-Console.WriteLine("Hello, World!");
+namespace SOHChristmasMarketBox;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("EN-US");
+        LoggerFactory.SetLogLevel(LogLevel.Off);
+
+        var description = new ModelDescription();
+        description.AddLayer<SpatialGraphMediatorLayer>(new[] { typeof(ISpatialGraphLayer) });
+        
+        description.AddLayer<HumanTravelerLayer>();
+        description.AddLayer<AgentSchedulerLayer<HumanTraveler, HumanTravelerLayer>>(
+            "HumanTravelerSchedulerLayer");
+
+        description.AddAgent<HumanTraveler, HumanTravelerLayer>();
+
+        ISimulationContainer application;
+        if (args != null && args.Length != 0)
+        {
+            var container = CommandParser.ParseAndEvaluateArguments(description, args);
+            var config = container.SimulationConfig;
+            application = SimulationStarter.BuildApplication(description, config);
+        }
+        else
+        {
+            var file = File.ReadAllText("config.json");
+            var simConfig = SimulationConfig.Deserialize(file);
+            application = SimulationStarter.BuildApplication(description, simConfig);
+        }
+
+        var simulation = application.Resolve<ISimulation>();
+        
+        var watch = Stopwatch.StartNew();
+        var state = simulation.StartSimulation();
+
+        watch.Stop();
+
+        Console.WriteLine($"Executed iterations {state.Iterations} lasted {watch.Elapsed}");
+        application.Dispose();
+    }
+}
