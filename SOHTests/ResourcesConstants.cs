6119e80185d2b21c89cb08595d6789d7640aef68
SimSon <wui756@haw-hamburg.de>
2024-12-13 14:11:10 +0100

    Add BigEvent bus layer test class and related resources

--- SOHTests/ResourcesConstants.cs
+++ SOHTests/ResourcesConstants.cs
@@ -22,6 +22,7 @@ public static class ResourcesConstants
     public static readonly string TrainCsv = Path.Combine(EntityInitsFolder, "train.csv");
     public static readonly string BusCsv = Path.Combine(EntityInitsFolder, "bus.csv");
     public static readonly string Bus113LineCsv = Path.Combine(EntityInitsFolder, "bus_113_line.csv");
+    public static readonly string BigEventLinesCsv = Path.Combine(EntityInitsFolder, "big_event_lines.csv");
     public static readonly string FerryLineCsv = Path.Combine(EntityInitsFolder, "ferry_line.csv");
     public static readonly string TrainU1LineCsv = Path.Combine(EntityInitsFolder, "train_u1_line.csv");
     public static readonly string FerryDriverCsv = Path.Combine(AgentInitsFolder, "ferry_driver.csv");
@@ -65,6 +66,9 @@ public static class ResourcesConstants
     public static readonly string Bus113Graph =
         Path.Combine(NetworkFolder, "hamburg_bus_113_graph.geojson");
 
+    public static readonly string BigEventBusGraph =
+        Path.Combine(NetworkFolder, "big_event_bus_graph.geojson");
+
     public static readonly string FerryContainerWalkingGraph =
         Path.Combine(NetworkFolder, "hamburg_south_graph_filtered.geojson");
 
@@ -92,6 +96,9 @@ public static class ResourcesConstants
 
     public static readonly string BusStations113 =
         Path.Combine(VectorDataFolder, "gateway", "hamburg_bus_113_stations.geojson");
+
+    public static readonly string BigEventBusStations =
+        Path.Combine(NetworkFolder, "big_event_bus_graph.geojson");
 
     public static readonly string FerryStations =
         Path.Combine(VectorDataFolder, "gateway", "hamburg_ferry_stations.geojson");
