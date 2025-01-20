57a9a76d2e11e1a35a8e0a299b69d76316d4c68b
SimSon <wui756@haw-hamburg.de>
2025-01-20 15:12:18 +0100

    Add functionality for bus using visitors to take a random line

--- SOHModel/BigEvent/Visitor.cs
+++ SOHModel/BigEvent/Visitor.cs
@@ -2,6 +2,7 @@ using Mars.Common.Core.Random;
 using Mars.Interfaces.Annotations;
 using Mars.Interfaces.Environments;
 using SOHModel.Bicycle.Parking;
+using SOHModel.Bus.Station;
 using SOHModel.Multimodal.Model;
 
 
@@ -15,7 +16,14 @@ public class Visitor : Traveler<BaseWalkingLayer>
     private ISet<ModalChoice> _choices;
     private ModalChoice _preferred;
     private readonly Random _random = new Random();
-    
+
+    private static readonly Dictionary<string, string[]> _stationsByLine = new()
+    {
+        { "180", new[] { "de:02000:84032::840015", "de:02000:84030::840093" } },
+        { "380(Shuttle)", new[] { "de:02000:84008::1101","de:02000:21000::2100" } },
+        { "22", new[] { "de:02000:84050::840181", "de:02000:84030::840093"} }
+    };
+
     [PropertyDescription] public IBicycleParkingLayer BicycleParkingLayer { get; set; }
 
     /**
@@ -27,10 +35,10 @@ public class Visitor : Traveler<BaseWalkingLayer>
         OvertakingActivated = true;
         _choices = new ModalityChooser().Evaluate(this);
         _choices.Add(ModalChoice.Walking);
-        
+
         HandleLogic();
         const int radiusInM = 1000;
-        
+
         if (_choices.Contains(ModalChoice.CyclingOwnBike) && BicycleParkingLayer != null)
         {
             Bicycle = BicycleParkingLayer.CreateOwnBicycleNear(StartPosition, radiusInM, 1.0);
@@ -50,10 +58,10 @@ public class Visitor : Traveler<BaseWalkingLayer>
     {
         if (BarclaysParkingLayer.ParkedCars.Count > 0)
         {
-           var randomIndex = _random.Next(BarclaysParkingLayer.ParkedCars.Count);
-                   var randomCar = BarclaysParkingLayer.ParkedCars[randomIndex];
-                   BarclaysParkingLayer.ParkedCars.RemoveAt(randomIndex);
-                   return randomCar; 
+            var randomIndex = _random.Next(BarclaysParkingLayer.ParkedCars.Count);
+            var randomCar = BarclaysParkingLayer.ParkedCars[randomIndex];
+            BarclaysParkingLayer.ParkedCars.RemoveAt(randomIndex);
+            return randomCar;
         }
         throw new ArgumentException("No cars available in the Barclays parking layer. Please ensure that there are " +
                                     "maximum 3660 car drivers in the simulation.");
@@ -74,17 +82,19 @@ public class Visitor : Traveler<BaseWalkingLayer>
             { ModalChoice.Train, UsesTrain },
             { ModalChoice.CyclingOwnBike, UsesBike }
         };
-        
+
         var filteredChoices = _choices
             .Where(choice => modalProbabilities.ContainsKey(choice))
             .ToDictionary(choice => choice, choice => modalProbabilities[choice]);
-        
+
         if (filteredChoices.Any())
         {
             _preferred = filteredChoices
-                .OrderByDescending(kvp => kvp.Value) 
-                .First().Key; 
-        } else {
+                .OrderByDescending(kvp => kvp.Value)
+                .First().Key;
+        }
+        else
+        {
             _preferred = ModalChoice.Walking;
         }
     }
@@ -95,25 +105,71 @@ public class Visitor : Traveler<BaseWalkingLayer>
     }
 
 
+
     protected override MultimodalRoute FindMultimodalRoute()
     {
-        
+        if (_preferred.Equals(ModalChoice.Bus)) // If the preferred modality is the bus, randomly choose a bus line with corresponding start and end station and find a multimodal route.
+        {
+            BusStation[] stations = GetRandomStartAndEndStation();
+            return FindMultimodalRoute(stations[0], stations[1]);
+        }
         return MultimodalLayer.Search(this, StartPosition, GoalPosition, _preferred);
-        
     }
-    
+
+
+    /**
+     * This method finds a multimodal route between the start and the goal destination of a visitor using the bus.
+     * As the default bus route algorithm only finds corresponding bus stations by distance (as of 01/2025), this method takes to bus stations with intersecting lines as input.
+     * It then builds a multimodal route from the start to the end destination by first walking to the start station, then taking the bus to the end station and finally walking to the goal destination.
+    */
+    private MultimodalRoute FindMultimodalRoute(BusStation startStation, BusStation endStation)
+    {
+        if (!startStation.Lines.Intersect(endStation.Lines).Any()) throw new ArgumentException("The start and end station do not share a common line.");
+        MultimodalRoute routeToStartStation = MultimodalLayer.Search(this, StartPosition, startStation.Position, ModalChoice.Walking);
+        MultimodalRoute busRouteBetweenStations = MultimodalLayer.Search(this, startStation.Position, endStation.Position, ModalChoice.Bus);
+        MultimodalRoute routeFromEndStation = MultimodalLayer.Search(this, endStation.Position, GoalPosition, ModalChoice.Walking);
+
+        AppendMultiModalRoute(routeToStartStation, busRouteBetweenStations);
+        AppendMultiModalRoute(routeToStartStation, routeFromEndStation);
+
+        return routeToStartStation;
+    }
+
+    private void AppendMultiModalRoute(MultimodalRoute route1, MultimodalRoute route2)
+    {
+        foreach (RouteStop routeStop in route2.Stops)
+        {
+            route1.Add(routeStop.Route, routeStop.ModalChoice);
+        }
+    }
+
+    private BusStation[] GetRandomStartAndEndStation()
+    {
+        if (BusStationLayer == null)
+        {
+            throw new ArgumentException("BusStationLayer is null. Please ensure that the BusStationLayer is set.");
+        }
+        var randomIndex = _random.Next(_stationsByLine.Count);
+        var line = _stationsByLine.Keys.ElementAt(randomIndex);
+        if (BusStationLayer is BusStationLayer busStationLayer)
+        {
+            return [.. _stationsByLine[line].Select(busStationLayer.Find)];
+        }
+        throw new ArgumentException("BusStationLayer is not of type BusStationLayer. Please ensure that the BusStationLayer is set correctly.");
+    }
+
 
     #region input
 
     [PropertyDescription(Name = "usesBike")]
     public double UsesBike { get; set; }
 
-    [PropertyDescription(Name = "usesCar")] 
+    [PropertyDescription(Name = "usesCar")]
     public double UsesCar { get; set; }
-    
+
     [PropertyDescription(Name = "usesTrain")]
     public double UsesTrain { get; set; }
-    
+
     [PropertyDescription(Name = "usesBus")]
     public double UsesBus { get; set; }
 
@@ -133,10 +189,10 @@ public class ModalityChooser
 
         if (RandomHelper.Random.NextDouble() < attributes.UsesTrain)
             choices.Add(ModalChoice.Train);
-        
+
         if (RandomHelper.Random.NextDouble() < attributes.UsesBus)
             choices.Add(ModalChoice.Bus);
-        
+
         return choices;
     }
 }
