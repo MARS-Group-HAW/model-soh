197499b049fd0e888e5fc9ec5951b189cd6b8bf9
Jakob Schwehn <schwehn42@gmail.com>
2026-01-05 13:07:40 +0100

    add SOHTruckFuelAlternativeBox project with simulation configuration and program entry point

---
+++ SOHTruckFuelAlternativeBox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using Mars.Common.Core.Logging;
+using Mars.Components.Starter;
+using Mars.Core.Simulation;
+using Mars.Interfaces;
+using Mars.Interfaces.Model;
+using SOHModel.SemiTruck.Model;
+using SOHModel.SemiTruck.Scheduling;
+
+namespace TruckFuelAlternativeBox;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        var watch = Stopwatch.StartNew();
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("EN-US");
+        LoggerFactory.SetLogLevel(LogLevel.Info);
+        var description = new ModelDescription();
+
+
+        description.AddLayer<SemiTruckLayer>();
+        description.AddLayer<SemiTruckSchedulerLayer>();
+
+        description.AddAgent<SemiTruckDriver, SemiTruckLayer>();
+        // Add semi-truck-related entities
+        description.AddEntity<SemiTruck>();
+
+        ISimulationContainer application;
+        if (args.Length != 0)
+        {
+            Console.WriteLine("default config");
+            var container = CommandParser.ParseAndEvaluateArguments(description, args);
+            var config = container.SimulationConfig;
+            application = SimulationStarter.BuildApplication(description, config);
+        }
+        else
+        {
+            var configPath = Environment.GetEnvironmentVariable("MARS_CONFIG_PATH") ?? "config_GeoJSON.json";
+            Console.WriteLine("mars config path: {0}", configPath);
+            var file = File.ReadAllText(configPath);
+            var simConfig = SimulationConfig.Deserialize(file);
+
+            application = SimulationStarter.BuildApplication(description, simConfig);
+        }
+
+        var simulation = application.Resolve<ISimulation>();
+
+        var state = simulation.StartSimulation();
+
+        watch.Stop();
+
+        Console.WriteLine($"Executed iterations {state.Iterations} lasted {watch.Elapsed}");
+        application.Dispose();
+    }
+}
